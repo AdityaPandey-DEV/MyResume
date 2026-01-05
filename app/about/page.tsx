96069c8c02bb58a623a0b276402181d927146c24
Aditya Pandey <adityapandey.dev.in@gmail.com>
2026-01-05 21:14:43 +0530

    done changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,11 @@
 import Header from '@/components/Header'
 import About from '@/components/AboutMe/index'
 import Footer from '@/components/Footer'
+import { Metadata } from 'next'
+
+export const metadata:Metadata={
+  title:"About Me"
+}
 
 export default function AboutPage() {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Providers } from './providers'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Aditya Pandey - Portfolio',
+  title: 'Aditya Pandey',
   description: 'B.Tech CSE Student & Full Stack Developer',
 }
 
